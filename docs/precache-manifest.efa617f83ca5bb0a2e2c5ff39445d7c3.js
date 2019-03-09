5fef87c15c7f0662a35e80a3379740b5c9856eac
albahniuk <albalopezfolgar@gmail.com>
2019-03-09 20:12:55 +0100

    add docs

---
+++ docs/precache-manifest.efa617f83ca5bb0a2e2c5ff39445d7c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a15534290c3f204fe966",
+    "url": "./static/css/main.29bc6f1f.chunk.css"
+  },
+  {
+    "revision": "a15534290c3f204fe966",
+    "url": "./static/js/main.a1553429.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "7d1851d81603a87ff103",
+    "url": "./static/js/2.7d1851d8.chunk.js"
+  },
+  {
+    "revision": "16699c3ad765a4a3408f45276e55dbb5",
+    "url": "./index.html"
+  }
+];
